353ae584826b5174bdd9f2ba6511f7c22b3de6c4
DarKhan <Darkhan0k0@yandex.kz>
2026-01-11 20:31:20 +0500

    Готовый проект v1.2

--- src/main.js
+++ src/main.js
@@ -81,7 +81,7 @@ function analyzeSalesData(data, options) {
     }));
 
     // @TODO: Индексация продавцов и товаров для быстрого доступа
-    const sellerIndex = Object.fromEntries(sellerStats.map(s => [s.seller_id, s]));
+    const sellerIndex = Object.fromEntries(sellerStats.map(s => [s.sellerId, s]));
 
     const productIndex = Object.fromEntries(data.products.map(prod => [prod.sku, prod]));
 

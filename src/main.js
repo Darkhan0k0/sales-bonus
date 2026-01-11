6344d006280802f589f7f902d0eb4f10ce0ca865
DarKhan <Darkhan0k0@yandex.kz>
2026-01-11 21:35:10 +0500

    Готовый проект v2.5

--- src/main.js
+++ src/main.js
@@ -92,15 +92,15 @@ function analyzeSalesData(data, options) {
             seller.revenue += record.total_amount - record.total_discount;
         }
         record.items.forEach(item => {
-            const product = productIndex[item.sku];// товар
+            const product = productIndex[item.sku];
 
             
-            const cost = product.purchase_price * item.quantity;// Посчитать себестоимость
+            const cost = product.purchase_price * item.quantity;
             const revenue = calculateRevenue(item, product);
             const profit = revenue - cost;
             seller.profit += profit;
 
-        if (!seller.products_sold[item.sku]) {/////АААААААААА
+        if (!seller.products_sold[item.sku]) {
             seller.products_sold[item.sku] = 0;
 }
             seller.products_sold[item.sku] += item.quantity;
